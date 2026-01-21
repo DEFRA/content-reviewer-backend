4095af113c25a92f150c3abba6ea5892ec8bf1d4
Santha Meena <141783879+cog-meenss@users.noreply.github.com>
2026-01-21 00:15:54 +0000

    Register results routes plugin

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -2,6 +2,7 @@ import { health } from '../routes/health.js'
 import { example } from '../routes/example.js'
 import { uploadRoutes } from '../routes/upload.js'
 import { reviewRoutes } from '../routes/review.js'
+import { results } from '../routes/results.js'
 import { sqsWorkerStatus } from '../routes/sqs-worker-status.js'
 import { chatController, reviewController } from '../routes/chat.js'
 
@@ -25,7 +26,7 @@ const router = {
 
       // Other routes
       server.route([health, sqsWorkerStatus].concat(example))
-      await server.register([uploadRoutes, reviewRoutes])
+      await server.register([uploadRoutes, reviewRoutes, results])
     }
   }
 }
