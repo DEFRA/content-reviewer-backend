459780e1db722c52bad97b1e43363752e6c09a35
anas-ghan <anas.ghanbasha@cognizant.com>
2026-02-23 11:56:17 +0000

    Removed excessive logs

--- src/common/helpers/sqs-client.js
+++ src/common/helpers/sqs-client.js
@@ -102,15 +102,6 @@ class SQSClientHelper {
       const result = await this.sqsClient.send(command)
       const duration = Math.round(performance.now() - startTime)
 
-      logger.info({
-        messageId: result.MessageId,
-        uploadId: messageData.uploadId,
-        reviewId: messageData.reviewId,
-        queueUrl: this.queueUrl,
-        queueName: this.queueName,
-        durationMs: duration
-      })
-
       return {
         success: true,
         messageId: result.MessageId,
